90ef74acec164c02d304d1d66d0f7c4a6f27e366
AgentFabulous <kshitijgm@gmail.com>
2022-10-24 18:41:36 +0200

    fleur/blobs: Import IMS blobs
    * Import IMS list blobs from camellia's tree
    
    Co-Authored-By: Woomymy <woomy@woomy.be>
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -184,6 +184,16 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.fleur \
     android.hardware.health-service.fleur-recovery
 
+# IMS
+PRODUCT_BOOT_JARS += \
+    mediatek-common \
+    mediatek-framework \
+    mediatek-ims-base \
+    mediatek-ims-common \
+    mediatek-telecom-common \
+    mediatek-telephony-base \
+    mediatek-telephony-common
+    
 # Init
 PRODUCT_PACKAGES += \
     init.cgroup.rc \
