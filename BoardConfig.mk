86bf41a8f7201a752e34de109a220d5630bd278f
Woomymy <woomy@woomy.be>
2023-02-26 18:18:11 +0100

    fleur: power: Switch to android.hardware.power-service-mediatek from hardware/mediatek
    * DT2W is not implemented in power-ext.cpp
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,9 @@ BOARD_AVB_VBMETA_VENDOR_ALGORITHM := SHA256_RSA4096
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX := 1
 BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 
+# Power HAL Ext
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-ext.cpp
+
 # SELinux
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
