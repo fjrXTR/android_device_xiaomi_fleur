10f15fc3b4a8f4a40574dcc34e7ae791ccc22e93
bengris32 <bengris32@protonmail.ch>
2023-05-19 14:53:38 +0100

    fleur: Build GNSS service from source
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I4490df798a5fbba4c34b3b07545551f3dd043fd5
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -179,14 +179,7 @@ PRODUCT_PACKAGES += \
 
 # Gnss
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0.vendor \
-    android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.0.vendor \
-    android.hardware.gnss@2.1.vendor \
-    android.hardware.gnss.measurement_corrections@1.0.vendor \
-    android.hardware.gnss.measurement_corrections@1.1.vendor \
-    android.hardware.gnss.visibility_control@1.0.vendor \
-    android.hardware.gnss-V1-ndk.vendor
+    android.hardware.gnss-service.mediatek
 
 # Health
 PRODUCT_PACKAGES += \
