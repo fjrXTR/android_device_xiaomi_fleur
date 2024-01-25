4626a336403d587d35d6372a007297776bdee668
fjrXTR <fajarslebew31@gmail.com>
2024-01-25 08:30:23 +0000

    fleur: Build other common libs from source
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>

--- device.mk
+++ device.mk
@@ -248,6 +248,12 @@ PRODUCT_PACKAGES += \
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+# Other Common libs
+PRODUCT_PACKAGES += \
+    libruy.vendor \
+	libmemunreachable \
+	libmemunreachable.vendor
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0.vendor \
